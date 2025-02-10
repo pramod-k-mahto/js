97e65d5aedc7aa872fa60e4721af26cae48b0720
Pramod Mahto <Mahtop324@gmail.com>
2025-02-10 10:28:24 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,130 @@
+// let const var
+
+//let age=9
+
+// s ,12
+// $ _  
+
+// name1
+
+// $nam
+// _name
+
+// fullName
+// data type 
+// primitive and non-primitive
+// primitive
+// String,Number,Boolean,Symbol,BigInt ,undefined null,
+// let a='hwgvbwuyuwyb97845938738777'
+// let isBool=false/true
+// let x=Symbol("Hello")
+// let y=Symbol("Hello")
+// console.log(x==y)
+// console.log(typeof a)
+//non-primitive
+// arr ,object
+// let num=374637461218756487564578468373453785638346374683746767468735748n
+// console.log(num)
+// undefined, null
+// let a;
+// // a=90
+// let b=null
+// console.log(typeof b)
+// console.log(a)
+// console.log(b)
+
+// non-primitive
+// arr ,object
+
+// let arr=[12,12.21,true,false,"Sjhbdvjsdvhbj"]
+// {
+
+//     0:12,
+//     1:12.21,
+//     2:true,
+//     3:false,
+//     4:"Sjhbdvjsdvhbj",
+// }
+
+// console.log(typeof arr)
+// console.log( arr[4])
+
+// const obje={
+//     name:"abc",
+//     phone:387453,
+// }
+
+// console.log(obje.name)
+// console.log(obje.phone)
+
+
+// Modulus (Remainder) %
+// console.log(1132123132121379%2)
+
+
+// ++ ,--
+
+
+// let x=10
+// console.log(++x)
+// console.log(--x)
+
+
+// JavaScript Assignment Operators
+// let y=12
+
+// y+=5
+// y-=5
+// y/=5
+// y%=5
+// y*=5
+
+// y**=2
+
+// console.log(y)
+
+
+// Logical Assignment Operators
+
+// and >&& 
+// or>> || 
+// !
+
+
+// cond1  && cond2
+//  true && true >>>true
+
+// cond1  || cond2
+//  true && true >>>true
+//  false && false >>>false
+
+// let x=9
+// let y=10
+// console.log(x==9 && y==10)
+// console.log(x==9 || y>10)
+// == ,===
+// let x=10;
+// let y="10"
+// console.log(x===y)
+
+// let x=6
+// console.log(x>=5)
+// console.log(x<=5)
+
+// let isBool = false;
+// console.log(!isBool)
+
+// let x=9
+// console.log(x?"hello world":"bye")
+// console.log(false+12+1+true)
+// let x=10;
+// if(x>5){
+//     console.log(`Hello${x}`)
+// }
+// else if(x<5){
+//     console.log("else if  ")
+// }
+// else{
+//     console.log("else")
+// }
+
